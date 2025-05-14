5a1bb7c421d60142eaefbfa6e54437b40a492961
DevoLaz <otongandmaddog@gmail.com>
2025-05-14 14:23:43 +0700

    iniers progressers akuers adamers

--- LabReservation/obj/Release/net8.0/LabReservation.AssemblyInfo.cs
+++ LabReservation/obj/Release/net8.0/LabReservation.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabReservation")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b873e2eb169bb241596265becc7e04d333f0e355")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabReservation")]

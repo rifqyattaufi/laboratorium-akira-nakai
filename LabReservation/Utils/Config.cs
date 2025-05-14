e05f17b3517a0bcba4bf1c4df4260cc881b9dec9
irgydaffa <irgytl12@gmail.com>
2025-05-14 12:27:45 +0700

    Config dan TimeValidator

---
+++ LabReservation/Utils/Config.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LabReservation.Utils
+{
+    public class Config
+    {
+        public int MaxReservationPerUser { get; set; }
+        public static Config Load(string path = "Data/config.json")
+        {
+            return File.Exists(path)
+                ? JsonSerializer.Deserialize<Config>(File.ReadAllText(path))!
+                : new Config { MaxReservationPerUser = 3 };
+        }
+    }
+}

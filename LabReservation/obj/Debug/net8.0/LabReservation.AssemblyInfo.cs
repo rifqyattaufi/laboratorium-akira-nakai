880f833c7a6ad11f287047ee9a48d6347f8f62ee
irgydaffa <irgytl12@gmail.com>
2025-05-14 21:37:39 +0700

    nambah config.json

--- LabReservation/obj/Debug/net8.0/LabReservation.AssemblyInfo.cs
+++ LabReservation/obj/Debug/net8.0/LabReservation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LabReservation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b873e2eb169bb241596265becc7e04d333f0e355")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+19a978cb3f2b7363c36bf51c12883e1db0ac8bb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LabReservation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LabReservation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
